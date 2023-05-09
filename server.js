c241652d3b25531ef1d1ca3b2dbc15c9630d8b03
hoholms <ninikabug@gmail.com>
2023-05-10 01:02:16 +0300

    Adjust x2

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files from the React app
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname)));
-app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')))
+
+// Serve static files from the node_modules directory
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules/')));
+
+// Serve the preline and aos files from their respective directories
+app.use('/preline', express.static(path.join(__dirname, '/node_modules/preline/dist/')));
+app.use('/aos', express.static(path.join(__dirname, '/node_modules/aos/dist/')));
+
+// Serve the Bootstrap Icons directory
+app.use('/icons', express.static(path.join(__dirname, '/node_modules/bootstrap-icons/font/')));
+
 
 const port = process.env.PORT || 3000;
 
