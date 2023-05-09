3055fe22ff1731f26042cc945e47d07d22a54da1
hoholms <ninikabug@gmail.com>
2023-05-09 23:07:26 +0300

    Initial Commmit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
